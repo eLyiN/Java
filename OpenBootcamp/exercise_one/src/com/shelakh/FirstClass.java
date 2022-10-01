b18ee47b75556f8f8a2cb31ef2566fed6a9ad578
Shelakh <staffultimaunion@gmail.com>
2022-10-01 20:25:07 +0200

    first commit

---
+++ OpenBootcamp/exercise_one/src/com/shelakh/FirstClass.java
@@ -0,0 +1,23 @@
+package com.shelakh;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        int var_0 = 1;
+        long var_1 = 1000;
+        double var_2 = 200d;
+        float var_3 = 300f;
+        boolean var_4 = true;
+        boolean var_5 = false;
+        char var_6 = 'Z';
+        String var_7 = "Me llamo Adrián.";
+
+        System.out.println("Esta es la variable numero 0: " + var_0);
+        System.out.println("Esta es la variable numero 1: " + var_1);
+        System.out.println("Esta es la variable numero 2: " + var_2);
+        System.out.println("Esta es la variable numero 3: " + var_3);
+        System.out.println("Esta es la variable numero 4: " + var_4);
+        System.out.println("Esta es la variable numero 5: " + var_5);
+        System.out.println("Esta es la variable numero 6: " + var_6);
+        System.out.println("Esta es la variable numero 7: " + var_7);
+    }
+}
