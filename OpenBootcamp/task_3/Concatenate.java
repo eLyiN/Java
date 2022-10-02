67bb514bce0e018a67a7fca14f035fd592a7d88c
Shelakh <staffultimaunion@gmail.com>
2022-10-02 15:57:39 +0200

    task 3 init

---
+++ OpenBootcamp/task_3/Concatenate.java
@@ -0,0 +1,11 @@
+public class    Concatenate
+{
+    public static void main(String[] args) {
+
+        String[] concatenated = new String[] {"a", "b", "c"};
+
+        for(int i = 0; i < concatenated.length; i++) {
+            System.out.print(concatenated[i]);
+        }
+    }
+}
